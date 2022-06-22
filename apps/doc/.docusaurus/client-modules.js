96a88e565d22a9e922489b1663ced3e38c978f13
Valentin RONTEIX <valentin.ronteix@ashudev.com>
2022-06-22 23:01:45 +0200

    feature: https and public expose overviewer, radarr, sonarr

---
+++ apps/doc/.docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require('/home/vyrtualsynthese/projects/ashuDev/homelab/apps/doc/node_modules/infima/dist/css/default/default.css'),
+  require('/home/vyrtualsynthese/projects/ashuDev/homelab/apps/doc/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/vyrtualsynthese/projects/ashuDev/homelab/apps/doc/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
+  require('/home/vyrtualsynthese/projects/ashuDev/homelab/apps/doc/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/home/vyrtualsynthese/projects/ashuDev/homelab/apps/doc/src/css/custom.css'),
+];
