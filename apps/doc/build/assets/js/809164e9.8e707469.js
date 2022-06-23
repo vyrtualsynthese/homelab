92a5cffe8d6d59b70c528e001787a0b87270fb01
Valentin RONTEIX <valentin.ronteix@ashudev.com>
2022-06-23 09:40:54 +0200

    refacto: cleanup doc build folder

--- apps/doc/build/assets/js/809164e9.8e707469.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkashulab_doc=self.webpackChunkashulab_doc||[]).push([[539],{3769:function(s){s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
