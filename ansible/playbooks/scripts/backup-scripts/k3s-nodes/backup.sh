f8482828b2eb07588a52e626cf3fa7e84b5dbb8c
Valentin RONTEIX <valentin.ronteix@ashudev.com>
2023-06-05 15:06:06 +0200

    :sparkles: Feat: Minio, NFS tweak, Ansible Tweak

--- ansible/playbooks/scripts/backup-scripts/k3s-nodes/backup.sh
+++ ansible/playbooks/scripts/backup-scripts/k3s-nodes/backup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+crictl rmi --prune
 restic backup /var/lib/rancher/k3s
-restic forget --keep-within-daily 7d --keep-within-weekly 1m --keep-within-monthly 1y
+restic forget --keep-within-daily 7d --keep-within-weekly 1m --prune
