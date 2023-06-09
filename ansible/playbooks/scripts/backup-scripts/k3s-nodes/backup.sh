9a30833cf1e4df90d99afb0e65621ee49eb29c02
Valentin RONTEIX <valentin.ronteix@ashudev.com>
2023-06-09 15:19:05 +0200

    :sparkles: Feat: Ansible refacto for backup to minio

--- ansible/playbooks/scripts/backup-scripts/k3s-nodes/backup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-crictl rmi --prune
-restic backup /var/lib/rancher/k3s
-restic forget --keep-within-daily 7d --keep-within-weekly 1m --prune
--- ansible/playbooks/scripts/backup-scripts/runner/backup.sh
+++ ansible/playbooks/scripts/backup-scripts/runner/backup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-restic backup /home/vronteix
+docker system prune -af
+restic backup /home/vronteix/dockers --exclude="gravity_old.db" --exclude="*.bak"
 restic forget --keep-within-daily 7d --keep-within-weekly 1m --prune
