74b7a53fd0ae4dd1d3637a548eb22877f53c7552
vyrtualsynthese <valentin.ronteix@ashudev.com>
2023-10-05 14:36:02 +0200

    :sparkles: Nextcloud restic backup

--- ansible/playbooks/scripts/backup-scripts/k3s/backup.sh
+++ ansible/playbooks/scripts/backup-scripts/k3s/backup.sh
@@ -2,6 +2,6 @@
 
 crictl rmi --prune
 sqlite3 /var/lib/rancher/k3s/server/db/state.db .dump > /var/lib/rancher/k3s/server/db/state.db.backup
-restic backup /var/lib/rancher/k3s/server/db/state.db.backup /var/lib/rancher/k3s/server/token
-restic forget  --keep-within-daily 7d --keep-within-weekly 1m --prune
+restic backup /var/lib/rancher/k3s/server/db/state.db.backup /var/lib/rancher/k3s/server/token -v
+restic forget  --keep-within-daily 7d --keep-within-weekly 1m --prune -v
 rm /var/lib/rancher/k3s/server/db/state.db.backup
---
+++ helm/nextcloud/backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if restic cat config >/dev/null 2>&1; then
+  echo 'repo already initialized'
+else
+  echo 'initializing repo'
+  restic init
+fi
+
+restic backup /data --host nextcloudbackup -v
+restic forget --keep-within-daily 7d --keep-within-weekly 1m --prune -v --host nextcloudbackup
